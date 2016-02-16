3e6537e45b750da53a6572236a9eac93319b6647
Irvan Aditya <itockz@gmail.com>
2016-02-16 22:14:57 +0700

    initial commit

---
+++ app/src/main/kotlin/com/anu/utanglist/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.anu.utanglist
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+
+/**
+ * Created by irvan on 2/16/16.
+ */
+class MainActivity: AppCompatActivity() {
+
+    var toolbar: Toolbar? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        toolbar = findViewById(R.id.toolbar) as Toolbar
+
+        setSupportActionBar(toolbar)
+    }
+}
