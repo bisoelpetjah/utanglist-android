6e8d66c05f8ab41e59a3dd92ca61a68bcc11f3ac
Irvan Aditya <itockz@gmail.com>
2016-02-26 19:52:09 +0700

    fix debt detail layout

--- app/src/main/kotlin/com/anu/utanglist/utils/PreferenceHelper.kt
+++ app/src/main/kotlin/com/anu/utanglist/utils/PreferenceHelper.kt
@@ -9,6 +9,8 @@ import android.preference.PreferenceManager
 object  PreferenceHelper {
 
     private final val PREF_ACCESS_TOKEN = "PREF_ACCESS_TOKEN"
+    private final val PREF_ECASH_ID = "PREF_ECASH_ID"
+    private final val PREF_ECASH_PIN = "PREF_ECASH_PIN"
 
     var context: Context? = null
 
@@ -19,6 +21,26 @@ object  PreferenceHelper {
         set(value) {
             PreferenceManager.getDefaultSharedPreferences(context).edit()
                     .putString(PREF_ACCESS_TOKEN, value)
+                    .commit()
+        }
+
+    var ecashId: String?
+        get() {
+            return PreferenceManager.getDefaultSharedPreferences(context).getString(PREF_ECASH_ID, null)
+        }
+        set(value) {
+            PreferenceManager.getDefaultSharedPreferences(context).edit()
+                    .putString(PREF_ECASH_ID, value)
+                    .commit()
+        }
+
+    var ecashPin: String?
+        get() {
+            return PreferenceManager.getDefaultSharedPreferences(context).getString(PREF_ECASH_PIN, null)
+        }
+        set(value) {
+            PreferenceManager.getDefaultSharedPreferences(context).edit()
+                    .putString(PREF_ECASH_PIN, value)
                     .commit()
         }
 }
